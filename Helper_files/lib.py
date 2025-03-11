9107a98b6e6c86bd1eab7910f7d316a932f2833f
Matthew Volkin <119533382+MattVolkin@users.noreply.github.com>
2025-03-11 14:48:25 +0000

    started a add folder function

--- Helper_files/lib.py
+++ Helper_files/lib.py
@@ -13,7 +13,15 @@ def file_to_list(filename):
         print("Error: File not found.")
         return []
 
-def ensure_file_exists(filename):
+def ensure_file_exists(filename,contents):
+    created = False
+    if not os.path.exists(filename):
+        with open(filename, 'w', encoding='utf-8') as file:
+            file.write(contents)  # Create an empty file
+        created = True
+    return created
+
+def ensure_folder_exists(foldername,files:list,contents:list ):
     created = False
     if not os.path.exists(filename):
         with open(filename, 'w', encoding='utf-8') as file:
