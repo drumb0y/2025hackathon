4e2abb1c93ae2ffb74cd3599f425b9d05d31a37f
Matthew Volkin <119533382+MattVolkin@users.noreply.github.com>
2025-03-22 16:13:42 +0000

    finished 2

--- Test_answer_file.py
+++ Test_answer_file.py
@@ -1,9 +1,9 @@
 import sys
 import os
-from Helper_files.lib import file_to_list, ensure_file_exists,ensure_folder_exists,exercise1
+from Helper_files.lib import file_to_list, ensure_file_exists,exercise1,exercise2
 
 def main():
-
+    count = 0 
     if(ensure_file_exists("answers.txt")):
         print("Welcome to the Digital Scavenger Hunt! A new file was just created called answers.txt. You can see the contents of it using the command 'cat' and then the file name.\nTry it and rerun this program!")
     else:
@@ -13,10 +13,16 @@ def main():
             ensure_file_exists("one.txt")
             print("Awesome! Now the real fun begins. Let's get started with the basics. A new file was just created. Enter the name of the file (without the file type) in the first line of answers.txt")
             sys.exit()
-        if user_answers[0] == answer_key[0]:
+        if user_answers[count] == answer_key[0]:
             print("Awesome now on to the next challenge")
             exercise1()
-        if user_answers[0] != answer_key[0]:
+            count+=1
+        if user_answers[count] != answer_key[0]:
+            print("uh oh")
+        if user_answers[count] == answer_key[1]:
+            print("Awesome now on to the next challenge")
+            exercise2()
+        if user_answers[count] != answer_key[1]:
             print("uh oh")
 
 if __name__ == "__main__":
