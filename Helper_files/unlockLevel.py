55c8eba15c6d8367aa639993b99b46ffc1e5ea7c
AidanBalakrishnan <balakrisa@southwestern.edu>
2025-06-18 16:57:02 -0500

    made unlockLevel use general file paths so it will work on any system

--- Helper_files/unlockLevel.py
+++ Helper_files/unlockLevel.py
@@ -1,18 +1,20 @@
 import os 
 import shutil
 
+parentDir = os.path.dirname(os.getcwd()) # directory that both player and helper files are in
 pathToBTSFiles = os.getcwd() # directory that has the all production files
-pathToPlayerFiles = "" # can change as needed but this is the folder that we can add files to as a player levels up
+pathToPlayerFiles = parentDir + "\playerfiles" # can change as needed but this is the folder that we can add files to as a player levels up
 currentLevel = 0 # counter to track player level
 playerName = "TEST TEAM" # 
 
 
-def start(): #start from here and create exercise 1
-    pathToBTSFiles = os.getcwd() # get the directory of the production/BTS files
-    os.system("cd ..")
-    print
+def start(): #start from here and create exercise 1    
     
+    print(parentDir)
+    print(pathToBTSFiles)
+    print(pathToPlayerFiles)
     
+    ### intro to hackathon
     print("Hello how are you doing, this sounds like a wonderful day\n")
     print("Secret says that the CS professors have locked their answerkeys for the midterm and finals behind all of these challenges\n")
     print("You have found the location of all the answerkeys, the data center in prothro, but unfortunatly \n")
@@ -22,20 +24,35 @@ def start(): #start from here and create exercise 1
 
 
 def unlockLevel(levelNumber) : # as the player leves up, 
+    print("unlocking level " + str(levelNumber))
+
     
-    if levelNumber == 8:
-        print("unlocking level 8")
+    ## for each level, run the according command to copy the required files to the players folder
+    if levelNumber == 1:
+        unlockLevel1()
+    elif levelNumber == 2:
+        unlockLevel2()
+    elif levelNumber == 3:
+        unlockLevel3()
+    elif levelNumber == 4:
+        unlockLevel4()
+    elif levelNumber == 5:
+        unlockLevel5()
+    elif levelNumber == 6:
+        unlockLevel6()
+    elif levelNumber == 7:
+        unlockLevel7()
+    elif levelNumber == 8:
         unlockLevel8()
         
         
         
+        
 def unlockLevel8() :
-    os.system(("mkdir " + (pathToPlayerFiles+"exercise8")))
+    os.system(("mkdir " + (pathToPlayerFiles+"\exercise8")))
     
     moveFileFromAtoB(pathToBTSFiles+"\exercise8\Exercise8descrition.txt", pathToPlayerFiles+"\exercise8\Exercise8descrition.txt")
 
-    
-    
     
 def moveFileFromAtoB(fileToBeMoved, endLocationFolder) :
     shutil.copyfile(fileToBeMoved, endLocationFolder)
@@ -43,4 +60,5 @@ def moveFileFromAtoB(fileToBeMoved, endLocationFolder) :
     
 if __name__ == "__main__":
     print(os.getcwd())
-    unlockLevel(8)
+    start()
+    #unlockLevel(8)
