fc73ba4d96e39334a22951d52834e023c175a570
Matthew Volkin <119533382+MattVolkin@users.noreply.github.com>
2025-06-26 14:26:16 -0500

    just need to write a checker for exercise 3

---
+++ Helper_files/exercise3/run3.py
@@ -0,0 +1,6 @@
+print("In order to pass this challenge you must do the following\n"
+    "1. Create a file named ABC.txt \n" 
+    "2. Write this in the file \n" 
+    "3. run randomLine.py \n" 
+    "4. Find and replace 75A8 with its binary represntation\n"
+    "5. Save and close the file")
