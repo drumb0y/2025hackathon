4d59fcbe7e77ac29195f3c33707113687bd26031
Matthew Volkin <119533382+MattVolkin@users.noreply.github.com>
2025-06-27 16:11:30 -0500

    added errors to the checker code

--- Helper_files/exercise3/testFile.py
+++ Helper_files/exercise3/testFile.py
@@ -7,4 +7,10 @@ def check():
             num = int(lines[0])
             if (lines[num -1] is "0111 0101 1010 1000" )or (lines[num -1]=="0111010110101000"):
                 print("Put this in the answer file: hqwlfjalfq65d")
+            else:
+                raise ValueError("binary not correct")
+        else:
+            raise ValueError("String not found in the file")
+    else:
+        raise FileNotFoundError()
 check()
