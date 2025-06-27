cd9786447a6dfd44d34b56420b151a56b140bb0a
Matthew Volkin <119533382+MattVolkin@users.noreply.github.com>
2025-06-27 16:04:26 -0500

    wrote checker for vim exercise

---
+++ Helper_files/exercise3/testFile.py
@@ -0,0 +1,10 @@
+import os 
+def check():
+    if os.path.isfile("ABC.txt"):
+        with open("ABC.txt", "r", encoding="utf-8") as file:
+            lines = file.readlines()        
+        if "Write this statement in the file \n" in lines:
+            num = int(lines[0])
+            if (lines[num -1] is "0111 0101 1010 1000" )or (lines[num -1]=="0111010110101000"):
+                print("Put this in the answer file: hqwlfjalfq65d")
+check()
